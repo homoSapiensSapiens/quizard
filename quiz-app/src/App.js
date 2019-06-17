57f9a315a6a43c16791a5f336a024b946ed201cb
Reuven Peleg <reuvenpe2005+code@gmail.com>
2019-06-17 22:38:37 +0300

    Beutification by a friend

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -6,8 +6,6 @@ import Container from '@material-ui/core/Container'
 class Question extends React.Component {
   constructor(props) {
     super(props);
-    const { onAnswerCallback } = props;
-    this.onAnswerCallback = onAnswerCallback;
     this.state = {
       selectedAnswer: -1
     }
@@ -18,61 +16,67 @@ class Question extends React.Component {
       selectedAnswer: answerId
     })
     if (this.onAnswerCallback) {
-      this.onAnswerCallback(this.props.id, answerId);
+      this.props.onAnswerCallback(this.props.id, answerId);
     }
   }
 
   render() {
+    const {selectedAnswer} = this.state;
+    const {answers} = this.props;
     const that = this;
     return <Container maxWidth='sm' justify='center'>
       {this.props.text}
-      <br/>
-      {this.props.answers.map((a, index) => {
-        const color = that.state.selectedAnswer === a.id ? 'primary' : 'default';
-        const clickCallback = (event) => that.onAnswerClick(event, a.id);
-        return <Button color={color} key={index} onClick={clickCallback}>{a.text}</Button>
-      })}
-    </Container>  
+      <br />
+      {answers.map(({id, text}) =>
+        <Button 
+          key={id} 
+          color={selectedAnswer === id ? 'primary' : 'default'} 
+          onClick={event => this.onAnswerClick(event, id)}
+        >{text}</Button>
+      )}
+    </Container>
   }
 }
 
-function Quiz(props) {
-  const questions = props.questions.map((q, index) => 
-    <Question text={q.text} answers={q.answers} key={index} id={q.id}
-    onAnswerCallback={(questionID, AnswerID) => console.log("Q:" + questionID + ", Answer: " +  AnswerID)}
-    />
-  )
-  return <Container color='green'>
-    {questions}
-  </Container>
+class Quiz extends React.Component {
+  render() {
+    const questions = this.props.questions.map((q, index) =>
+      <Question text={q.text} answers={q.answers} key={index} id={q.id}
+        onAnswerCallback={(questionID, AnswerID) => console.log("Q:" + questionID + ", Answer: " + AnswerID)}
+      />
+    )
+    return <Container color='green'>
+      {questions}
+    </Container>
+  }
 }
 
 function App() {
-  const quiz =  [
+  const quiz = [
     {
       id: 0,
       text: 'Foo bar baz?',
       answers: [
-        {id: 0, text: 'foo'},
-        {id: 1, text: 'bar'},
-        {id: 2, text: 'baz'}
+        { id: 0, text: 'foo' },
+        { id: 1, text: 'bar' },
+        { id: 2, text: 'baz' }
       ]
-    }, 
+    },
     {
       id: 1,
       text: 'What is better?',
       answers: [
-        {id: 0, text: 'To be'},
-        {id: 1, text: 'Not to be'}
+        { id: 0, text: 'To be' },
+        { id: 1, text: 'Not to be' }
       ]
     },
     {
       id: 2,
       text: 'Who killed Mufasa?',
       answers: [
-        {id: 0, text: 'Simba'},
-        {id: 1, text: 'Kiara'},
-        {id: 2, text: 'Scar'}
+        { id: 0, text: 'Simba' },
+        { id: 1, text: 'Kiara' },
+        { id: 2, text: 'Scar' }
       ]
     }
   ]
