2cab7b3d4d7f7fd66d4bfe6ed000136bddfeaaeb
Reuven Peleg <reuvenpe2005+code@gmail.com>
2019-06-17 21:23:24 +0300

    Add on answer callbacks and button marking
    This already seem nicer

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -3,19 +3,43 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container'
 
-function Question(props) {
-  return <Container maxWidth='sm' justify='center'>
-    {props.text}
-    <br/>
-    {props.answers.map((a, index) => 
-      <Button key={index}>{a}</Button>)
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    const { onAnswerCallback } = props;
+    this.onAnswerCallback = onAnswerCallback;
+    this.state = {
+      selectedAnswer: -1
     }
-  </Container>
+  }
+
+  onAnswerClick = (event, answerId) => {
+    this.setState({
+      selectedAnswer: answerId
+    })
+    if (this.onAnswerCallback) {
+      this.onAnswerCallback(this.props.id, answerId);
+    }
+  }
+
+  render() {
+    const that = this;
+    return <Container maxWidth='sm' justify='center'>
+    {this.props.text}
+    <br/>
+    {this.props.answers.map((a, index) => {
+      const color = that.state.selectedAnswer === a.id ? 'primary' : 'default';
+      const clickCallback = (event) => that.onAnswerClick(event, a.id);
+      return <Button color={color} key={index} onClick={clickCallback}>{a.text}</Button>
+    })}
+  </Container>  }
 }
 
 function Quiz(props) {
   const questions = props.questions.map((q, index) => 
-    <Question text={q.text} answers={q.answers} key={index}/>
+    <Question text={q.text} answers={q.answers} key={index}
+    onAnswerCallback={(questionID, AnswerID) => console.log("Q:" + questionID + ", Answer: " +  AnswerID)}
+    />
   )
   return <Container color='green'>
     {questions}
@@ -25,12 +49,21 @@ function Quiz(props) {
 function App() {
   const quiz =  [
     {
+      id: 0,
       text: 'Foo bar baz?',
-      answers: ['foo', 'bar', 'baz']
+      answers: [
+        {id: 0, text: 'foo'},
+        {id: 1, text: 'bar'},
+        {id: 2, text: 'baz'}
+      ]
     }, 
     {
+      id: 1,
       text: 'What is better?',
-      answers: ['To be', 'Not to be']
+      answers: [
+        {id: 0, text: 'To be'},
+        {id: 1, text: 'Not to be'}
+      ]
     }
   ]
 
