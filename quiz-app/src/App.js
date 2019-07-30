c41f6cf451955d4c248a08f6fdc5affaa28a72a9
Reuven Peleg <reuvenpe2005+code@gmail.com>
2019-07-30 07:19:35 +0300

    Add app-bar with link to main page

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import './App.css'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
 import QuizSession from './componenets/QuizSession'
 import RemoteFetchingQuiz from './componenets/RemoteFetchingQuiz';
 import QuizList from './componenets/QuizList';
-import { HashRouter, Route } from 'react-router-dom';
-import { Grid } from '@material-ui/core';
+import { HashRouter, Route, Link } from 'react-router-dom';
+import { Grid, MenuItem } from '@material-ui/core';
 
 function App() {
   return (
@@ -15,7 +18,18 @@ function App() {
       direction="column"
       alignItems="center"
     >
+
       <HashRouter>
+        <AppBar position="static">
+          <Toolbar>
+            <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
+              <MenuItem>
+                Main Page
+              </MenuItem>
+            </Link>
+          </Toolbar>
+        </AppBar>
+
         <Route path='/' exact component={() => <QuizList/>}/>
         <Route path='/quiz/0' component={() => 
           <QuizSession
