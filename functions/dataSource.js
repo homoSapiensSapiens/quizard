525ea68dd0d0a2e9a95f24e9f926ce25abd5d370
Reuven Peleg <reuvenpe2005+code@gmail.com>
2019-07-30 13:58:21 +0300

    Use indicative IDs everywhere

--- functions/dataSource.js
+++ functions/dataSource.js
@@ -1,58 +1,58 @@
 const quiz0 = {
   questions :[{
-    id: 0,
+    id: 'favorite_color',
     text: 'What is your favorite color?',
     answers: [
-      { id: 0, text: 'Red' },
-      { id: 1, text: 'Green' },
-      { id: 2, text: 'Also green but circle' }
+      { id: 'red', text: 'Red' },
+      { id: 'green', text: 'Green' },
+      { id: 'green_circle', text: 'Also green but circle' }
     ]
   }, {
-    id: 1,
+    id: 'like_tea',
     text: 'Do you like tea?',
     answers: [
-      { id: 0, text: 'Yes, sure!' },
-      { id: 1, text: 'Nah, not for me' }
+      { id: 'yes', text: 'Yes, sure!' },
+      { id: 'no', text: 'Nah, not for me' }
     ]
   }, {
-    id: 2,
+    id: 'how_strong',
     text: 'How strong are you?',
     answers: [
-      { id: 0, text: 'Very strong' },
-      { id: 1, text: 'Normal' },
-      { id: 2, text: 'Pretty weak actually' }
+      { id: 'very', text: 'Very strong' },
+      { id: 'normal', text: 'Normal' },
+      { id: 'weak', text: 'Pretty weak actually' }
     ]
   }],
 
   results: [ // TODO: indicative strings as id
     {
-      id: 0,
+      id: 'apple',
       title: 'Apple',
       description: 'You are a tasty piece of fruit, and can be proud of yourself'
     },
     {
-      id: 1,
+      id: 'pear',
       title: 'Pear',
       description: 'A pear is a nice and beutiful fruit'
     }
   ],
   
   effects: {
-    '0': {
-      '0': { resultId: 0, operator: '+', operand: 1 },
-      '1': { resultId: 1, operator: '+', operand: 1 },
-      '2': { resultId: 0, operator: '+', operand: 0.5 }
+    'favorite_color': {
+      'red': { resultId: 'apple', operator: '+', operand: 1 },
+      'green': { resultId: 'pear', operator: '+', operand: 1 },
+      'green_circle': { resultId: 'apple', operator: '+', operand: 0.5 }
     },
   
-    '1': {
-      '0': { resultId: 0, operator: '+', operand: 2 },
-      '1': { resultId: 0, operator: '-', operand: -1 }
+    'like_tea': {
+      'yes': { resultId: 'apple', operator: '+', operand: 2 },
+      'no': { resultId: 'apple', operator: '-', operand: -1 }
     },
   
-    '2': {
-      '0': { resultId: 1, operator: '+', operand: 1 },
-      '1': { resultId: 1, operator: '-', operand: 0.5 },
-      '2': { resultId: 0, operator: '+', operand: 1 }
+    'how_strong': {
+      'very': { resultId: 'pear', operator: '+', operand: 1 },
+      'normal': { resultId: 'pear', operator: '-', operand: 0.5 },
+      'weak': { resultId: 'apple', operator: '+', operand: 1 }
     }
   }  
 };
